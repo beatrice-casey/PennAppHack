3b3c78ba8bb4f0fe0a8dcc540b8d5a06d026f245
Beatrice Casey <bac041@bucknell.edu>
2020-09-11 21:12:41 -0400

    updating files

--- app/src/main/java/com/example/pennapphack/ParseApplication.java
+++ app/src/main/java/com/example/pennapphack/ParseApplication.java
@@ -1,4 +1,34 @@
 package com.example.pennapphack;
 
-public class ParseApplication {
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId, and server server based on the values in the back4app settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("rIRCY1wRqICInAMsNzJHU8JIejIPgj4AjFTwBVVj") // should correspond to Application Id env variable
+                .clientKey("mvofRtW5xCpqbUPmr4YBCgj2ggxoD6VtzlhkeWLi")  // should correspond to Client key env variable
+                        .server("https://parseapi.back4app.com").build());
+    }
 }
