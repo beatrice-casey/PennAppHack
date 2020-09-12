da92df9f092611462aed8510ddc55fe43e26f887
Jack Otto <jno003@bucknell.edu>
2020-09-12 05:13:41 -0400

    Create a Post Page

--- app/src/main/java/com/example/pennapphack/CreateFragment.java
+++ app/src/main/java/com/example/pennapphack/CreateFragment.java
@@ -1,58 +1,51 @@
 package com.example.pennapphack;
 
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CreateFragment#newInstance} factory method to
+ * Use the {@link CreateFragment #newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CreateFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String [] TIMELIST ={"30","60","90","120"};
+    String [] PRICELIST ={"$","$$","$$$"};
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button post;
+    private Button picture;
+    private EditText information;
+    private ImageView foodPic;
+    private TextView recipeName;
+    private Spinner timeSpin;
+    private Spinner priceSpin;
+    private Spinner timeSpinner;
+    private Spinner priceSpinner;
+
 
     public CreateFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateFragment newInstance(String param1, String param2) {
-        CreateFragment fragment = new CreateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -60,5 +53,28 @@ public class CreateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        post = view.findViewById(R.id.btnPost);
+        picture = view.findViewById(R.id.btnFood);
+        information = view.findViewById(R.id.etDescription);
+        foodPic = view.findViewById(R.id.foodPicture);
+        recipeName = view.findViewById(R.id.recipeName);
+        timeSpin = view.findViewById(R.id.timeDrop);
+        priceSpin = view.findViewById(R.id.priceDrop);
+
+        //time spinner
+        ArrayAdapter<String> arrayAdapterTime= new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, TIMELIST);
+        timeSpinner = (Spinner)timeSpin;
+        timeSpinner.setAdapter(arrayAdapterTime);
+
+        //price spinner
+        ArrayAdapter<String> arrayAdapterPrice = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, PRICELIST);
+        priceSpinner = (Spinner)priceSpin;
+        priceSpinner.setAdapter(arrayAdapterPrice);
     }
 }
